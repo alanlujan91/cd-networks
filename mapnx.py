7244b01bcf0b5c59815ac5d7bd653b17a5dd5aef
srijithprabhu <srijith.prabhu@utexas.edu>
2015-03-02 13:51:28 -0600

    Rounding the bounding box and points a little
    Initially we weren't getting hits because the bounding boxes didn't
    overlap. Added a little logic because the points would probably not be
    within the bbox if they don't overlap.

--- mapnx.py
+++ mapnx.py
@@ -10,6 +10,7 @@ import pandas as pd
 import networkx as nx
 #import matplotlib.pylab as plt
 from bbox import Bbox
+from math import ceil, floor, fabs
 
 class MapNetwork(object):
     
@@ -26,25 +27,46 @@ class MapNetwork(object):
             
             self.g = nx.Graph()
             self.g.position = {}
+            rounding_coefficient = 2.0
             for i in range(len(records)):
+                bbox = shapes[i].bbox
+                min_x = floor(rounding_coefficient * min(bbox[0], bbox[2])) / rounding_coefficient
+                min_y = floor(rounding_coefficient * min(bbox[1], bbox[3])) / rounding_coefficient
+                max_x = ceil(rounding_coefficient * max(bbox[0], bbox[2])) / rounding_coefficient
+                max_y = ceil(rounding_coefficient * max(bbox[1], bbox[3])) / rounding_coefficient
                 node = records[i][self.node_id]
+                shapes[i].bbox_round = [min_x,min_y, max_x, max_y]
                 self.g.add_node(node)
                 
+            state_index = columns.index("STATEFP")
             for i in range(len(self.g)):
                 shape_i = shapes[i]
                 district_i = records[i][self.node_id]
                 bbox_i = Bbox(shape_i.bbox)
                 self.g.position[district_i] = bbox_i.middle()
+                bbox_i = Bbox(shape_i.bbox_round)
                 print i
+                state_i = records[i][state_index]
                 for j in range(i+1,len(self.g)):
+                    if records[j][state_index] != state_i:
+                        continue
                     shape_j = shapes[j]
                     district_j = records[j][self.node_id]
-                    bbox_j = Bbox(shape_j.bbox)
+                    bbox_j = Bbox(shape_j.bbox_round)
                     if(bbox_j.isin(bbox_i)):
                         for p in shape_i.points:
                             if p in shape_j.points:
                                 self.g.add_edge(district_i, district_j)
                                 break
+                            else :
+                                contains = False
+                                for c in shape_j.points:
+                                    if fabs(p[0] - c[0]) <= 0.001 and fabs(p[1] - c[1]) <= 0.001:
+                                        self.g.add_edge(district_i, district_j)
+                                        contains = True
+                                        break
+                                if contains:
+                                    break
         else:
             self.g = g
             self.df = df
