908f81a29458b247eadc21b82fdbcb76f86e61bd
alanlujan91 <alanlujan91@gmail.com>
2015-03-03 22:40:52 -0500

    more shapes
    different classes for shapes

--- shapes.py
+++ shapes.py
@@ -5,6 +5,16 @@ Created on Sun Mar 01 14:17:31 2015
 @author: Alan
 """
 
+class Point(object):
+    
+    def __init__(self, coords):
+        self.x = coords[0]
+        self.y = coords[1]
+        
+    def dist(self, point):
+        return ((selfx - point.x)**2 + (self.y - point.y)**2)**0.5
+        
+
 class Shape(object):
     
     def __init__(self, shape):
@@ -12,3 +22,9 @@ class Shape(object):
         
     def round_pts(self, digits):
         return [[round(x, digits), round(y, digits)] for (x,y) in self.shape.points]
+        
+    def bbox(self):
+        return Bbox(self.shape.bbox)
+        
+    def isin(self, shape):
+        
