49e7bd7a8814c78966ee22caa985689f95bffced
alanlujan91 <alanlujan91@gmail.com>
2015-03-05 21:01:55 -0500

    mapnxdf example
    with us map

--- example mapnx.py
+++ example mapnx.py
@@ -5,9 +5,14 @@ Created on Sun Mar 01 01:05:03 2015
 @author: Alan
 """
 
-from mapnx import MapNetwork
+from mapnxdf import MapNetworkxDF
+from mpl_toolkits.basemap import Basemap
 
-mnx = MapNetwork('..\source\cb_2013_us_county_500k', 'GEOID')
+mnx = MapNetworkxDF(source = '..\source\cb_2013_us_county_500k.shp', key = 'GEOID')
 
 for state in mnx.df['STATEFP'].unique():
     mnx.filtered_graph('STATEFP', state).draw()
+    
+m = Basemap()
+m.drawstates(linewidth = 1, color = 'red')
+m.drawcounties(linewidth = 0.5, color = 'blue')
--- mapnxdf.py
+++ mapnxdf.py
@@ -13,26 +13,36 @@ from itertools import combinations
 
 class MapNetworkxDF(object):
     
-    def __init__(self, **kwargs):
+    def __init__(self, filtered = False, **kwargs):
         self.key = kwargs.get('key')
         self.source = kwargs.get('source')
-        self.g = nx.Graph()
+        self.g = kwargs.get('g')
+        self.df = kwargs.get('df')
         
-        with fiona.open(self.source) as nodes:
-            for node in nodes:
-                self.g.add_node(node['properties'][key])
-                
-            shapes = {node['properties'][key] : geom.shape(node['geometry']) for node in nodes}
+        if not filtered:
+            self.g = nx.Graph()
             
-            for u, v in combinations(self.g.nodes(), 2):
-                if shapes[u].touches(shapes[v]):
-                    self.g.add_edge(u, v)
+            with fiona.open(self.source) as nodes:
+                for node in nodes:
+                    self.g.add_node(node['properties'][self.key])
                     
-        self.df = pd.DataFrame.from_dict([node['properties'] for node in nodes])
+                shapes = {node['properties'][self.key] : geom.shape(node['geometry']) for node in nodes}
+                
+                for u, v in combinations(self.g.nodes(), 2):
+                    if shapes[u].touches(shapes[v]):
+                        self.g.add_edge(u, v)
+                        
+                self.g.pos = {key : shapes[key].centroid.coords[0] for key in shapes.keys()}
+                self.df = pd.DataFrame.from_dict([node['properties'] for node in nodes])
         
     def filtered_graph(self, attr, val): 
         df = self.df[self.df[attr] == val]
         g = nx.Graph()
-        [g.add_node(node) for node in self.g.nodes() if node in df[attr].values]
+        [g.add_node(node) for node in self.g.nodes() if node in df[self.key].values]
         [g.add_edge(u, v) for (u,v) in self.g.edges() if u in g and v in g]
+        g.pos = {node : self.g.pos[node] for node in g}
+        return MapNetworkxDF(filtered = True, g = g, df = df, key = self.key)
+        
+    def draw(self):
+        nx.draw_networkx(self.g, pos = self.g.pos, with_labels=False)
         
